2d93f1d68309a33dae8ebc49c18b7629c1a94f43
sajinskumar132 <sajinskumar132@gmail.com>
2023-11-11 22:06:34 +0530

    Add service Route

--- src/Routes/ServiceRoutes.ts
+++ src/Routes/ServiceRoutes.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { SignUp, login } from '../controllers/ServiceUserControllers/serviceUserController'
+import { NewService, getServices } from '../controllers/ServiceUserControllers/ServicesControllers'
 const ServiceRoutes=express.Router()
 
 ServiceRoutes.post('/login',login)
 ServiceRoutes.post('/signup',SignUp)
 
+ServiceRoutes.get('/services',getServices)
+ServiceRoutes.post('/newservice',NewService)
 export default ServiceRoutes
