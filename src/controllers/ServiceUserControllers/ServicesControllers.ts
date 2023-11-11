09a09560021da7539aee3820f3dc8a56f8314dff
sajinskumar132 <sajinskumar132@gmail.com>
2023-11-11 22:06:20 +0530

    Implemented newservice

--- src/controllers/ServiceUserControllers/ServicesControllers.ts
+++ src/controllers/ServiceUserControllers/ServicesControllers.ts
@@ -1,8 +1,8 @@
 
 import Service from "../../Models/ServiceProviders/Service";
-import jwt from 'jsonwebtoken'
 import { accessTokenAuthenticator } from "../../Services/accessTokenAuthenticator";
 import { RequestHandler } from "express";
+import { ServiceCatogory } from "../../util/Enums";
 export const getServices:RequestHandler=async(req,res,next)=>{
     try {
         const Services=await Service.find()
@@ -15,13 +15,25 @@ export const getServices:RequestHandler=async(req,res,next)=>{
 
 export const NewService:RequestHandler=async(req,res,next)=>{
     try {
-        const token=req.headers.authorization?.split(' ')[0]
+        const token=req.headers.authorization?.split(' ')[1]
         if(!token) return res.status(401).json({data:null,message:"Unauthorized"})
+        console.log(token)
         const userid = accessTokenAuthenticator.TokenAuthenticator(token)
+        console.log(userid)
         if(!userid) return res.status(401).json({data:null,message:"Unauthorized"})
-        const {category,experience,description,location,status}=req.body
-        
-
+        const {category,experience,description,location}=req.body
+        let IsValidCCategory=false
+        const keys = Object.keys(ServiceCatogory);
+        for(let item of  keys){
+            if(item == category){
+                IsValidCCategory=true
+                break
+            }
+        }
+        if(!IsValidCCategory) return res.status(400).json({data:null,message:"Invalid Category"})
+        const newService=new Service({userId:userid,category,experience,description,location,status:"Approved"})
+        await newService.save()
+        return res.status(201).json({data:newService,message:"Sucessfully created service"})
     } catch (error) {
         next(error)
         return res.status(500).json({data:null,message:"Internal Server error"})
